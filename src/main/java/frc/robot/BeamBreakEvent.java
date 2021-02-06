c01db13aa6d5ef54f8a9f292961593004e569eeb
xavier2910 <x.distelzweig@gmail.com>
2021-02-06 10:40:37 -0600

    added Ultrasonic constants

--- src/main/java/frc/robot/BeamBreakEvent.java
+++ src/main/java/frc/robot/BeamBreakEvent.java
@@ -1,33 +1,32 @@
 package frc.robot;
 
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard; 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class BeamBreakEvent {
     private static BeamBreakEvent event = null;
     private static int count = 0;
-    
+
     private BeamBreakEvent() {
     }
-    
+
     public static void trigger() {
-        if(event == null) {
+        if (event == null) {
             event = new BeamBreakEvent();
             ++count;
             SmartDashboard.putNumber("BeamBrokenTimes", count);
         }
     }
-        
-    
+
     public static void acknowledge() {
         if (event != null) {
             event = null;
         }
     }
 
-
     public static boolean checkIfTriggered() {
         return event != null;
     }
+
     public static int getNumberOfTimesTriggered() {
         return count;
     }
