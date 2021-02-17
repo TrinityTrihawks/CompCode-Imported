6c26f88bfec0544e27835e09cef65f49a06076d3
Anthony6444 <anthonydc1972@outlook.com>
2021-02-16 19:14:56 -0600

    edit

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,7 +7,6 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import jdk.jfr.Enabled;
 
 import static frc.robot.Constants.NetworkConstants;
 
